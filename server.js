0a0e8b181579829c97c155d4743626976eb80ce8
Logan J Daniels <danie855@umn.edu>
2016-02-19 22:27:34 -0600

    Add basic communication protocl to send state between clients and server

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 server.listen(8080);
 
-app.get('/', function (req, res) {
-  return;
+var nextClientId = 0;
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
 });
 
+app.get('/public/app.js', function(req, res){
+  res.sendfile('app.js');
+});
+
+app.use(express.static('public'));
+
 io.on('connection', function (socket) {
-  socket.emit('test', "Hello world!");
-  socket.on('h2f', function (data) {
+  console.log("Client connected.");
+  players[nextClientId] = {};
+  socket.emit('id', { id: nextClientId++});
+  socket.on('clientUpdate', function (data) {
+  	// Reconcile client update with server state
     console.log(data);
+    var playerState = players[data['id']];
+    playerState['x'] = data['x'];
+    playerState['y'] = data['y'];
   });
 });
+
+var loop = function() {
+	io.emit('update', state);
+	setTimeout(loop, 1000);
+}
+
+var state = {
+	players: {},
+}
+
+loop();
